76fb02f902dc4b3f8d72ffc9c1fb14c59485cd50
Gui <168383419+Guilherme220501@users.noreply.github.com>
2024-10-05 14:03:14 +0000

    adicionando primeira página php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Olá mundo !!</title>
+</head>
+<body>
+    <?php
+$numero2 = 33;
+$numero30 = 60;
+$soma = $numero2 + $numero30; ?>
+<p>A soma de 33 + 60 é: <?php echo $soma ?></p>
+</body>
+</html>
