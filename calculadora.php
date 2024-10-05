8f2dcf79b6484cf5ba7771f35686f2e98852926b
Gui <168383419+Guilherme220501@users.noreply.github.com>
2024-10-05 14:45:09 +0000

    adicionar calculadora

---
+++ calculadora.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Calculadora</title>
+</head>
+<body>
+    <h1><big>Calculadora Full-stack</big></h1>
+
+    <form action="calculadora.php" method="post">
+    <label for="Numero">Numero1</label>
+    <input type="number" name="Numero1" id="Numero1">
+    <label for="Numero">Numero2</label>
+    <input type="number" name="Numero1" id="Numero1">
+    <input type="submit" value="Calcular">
+    </form>
+    <hr>
+    <p>Resultado da operação</p>
+
+    <?php
+    if(empty)
+    >
+</body>
+</html>
